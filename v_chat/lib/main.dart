90f89fabd9182007041b24864c238ca431bbb5df
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-11-06 21:09:29 -0700

    got rid of red squiggles in main

--- v_chat/lib/main.dart
+++ v_chat/lib/main.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 import 'LoginPage.dart';
 
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
 
   Widget build(BuildContext context){
 
 
-    return new MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
