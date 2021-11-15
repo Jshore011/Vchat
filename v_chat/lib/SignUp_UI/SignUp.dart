f2b8017d226a2eec4c120b3c281726716fc70ea1
jessica Shore <shore011@cougars.csusm.edu>
2021-11-15 14:09:59 -0800

    bare bones login screen for communication from front end to back end

---
+++ v_chat/lib/SignUp_UI/SignUp.dart
@@ -0,0 +1,45 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class SignUpSection extends StatelessWidget {
+  var email;
+  var password;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          automaticallyImplyLeading: false,
+        ),
+        child: SafeArea(
+            child: ListView(padding: const EdgeInsets.all(16), children: [
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: CupertinoTextField(
+                      placeholder: "Email",
+                      onChanged: (value) {
+                        email = value;
+                      })),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: CupertinoTextField(
+                      placeholder: "Password",
+                      obscureText: true,
+                      onChanged: (value) {
+                        password = value;
+                      })),
+              FlatButton.icon(
+                  onPressed: () {
+                    print(email);
+                    print(password);
+                    signup(email, password);
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text("Sign UP"))
+            ])));
+  }
+}
--- v_chat/lib/main.dart
+++ v_chat/lib/main.dart
@@ -1,28 +1,58 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:v_chat/wrapper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
-import 'LoginUI/LoginPage.dart';
-//import 'SignupUI/SignupPage.dart';
+import 'SignUp_UI/SignUp.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
-  //const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  //State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignUpSection(),
+    );
+  }
+}
+
+void getHttp() async {
+    try {
+      var response = await Dio().get('http://34.204.3.211');
+      print(response);
+    } catch(e) {
+      print(e);
+    }
+}
+//getUserApi(email, password) async {
+  //var httpClient = new HttpClient();
+  //var uri = new Uri.https('yourserverurl.com', '/your/endpoint/whatever');
+  //var request = await httpClient.getUrl(uri);
+  //var response = await request.close();
+  //var responseBody = await response.transform(UTF8.decoder).join();
+  //return response;
 //}
 
-//class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context){
+signup(email, password) async {
+  var url =new Uri.https( 'yourserverurl.com', '/your/endpoint/whatever'); // iOS
+  final http.Response response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+      'password': password,
+    }),
+  );
 
-
-    return MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      home: wrapper(),
-
-
-    );
+  if (response.statusCode == 201) {
+  } else {
+    throw Exception('Failed to create user.');
   }
 }
