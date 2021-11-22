e7c3c38e55cc15e00db3d53e2da37838138e0971
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-11-22 15:22:42 -0800

    updated db data for test user

--- CordovaVersion/db_con.py
+++ CordovaVersion/db_con.py
@@ -2,7 +2,7 @@ import psycopg2
 
 
 def get_db():
-    return psycopg2.connect(host="localhost", dbname="test" , user="test", password="password123")
+    return psycopg2.connect(host="localhost", dbname="authme" , user="test", password="password123")
 
 def get_db_instance():  
     db  = get_db()
