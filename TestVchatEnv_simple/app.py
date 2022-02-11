107d05721ff253e43f2f2f0624e3570659459d80
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2022-02-10 19:13:18 -0800

    trying something new....

--- TestVchatEnv_simple/app.py
+++ TestVchatEnv_simple/app.py
@@ -1,44 +1,26 @@
-from flask import Flask,render_template,request, redirect, url_for, g, request
-from flask_json import FlaskJSON, JsonError, json_response, as_json
-import jwt
-
-import sys
-import datetime
-import bcrypt
-import traceback
-
-import firebase_admin
 import pyrebase
-import json
-from firebase_admin import credentials, auth
-
-import psycopg2
-from psycopg2 import sql
-
-ERROR_MSG = "That Didn't work!"
-
-#Create our app
+from flask import *
 app = Flask(__name__)
 
-#add in flask json
-FlaskJSON(app)
-
 #connect to firebase
-cred = credentials.Certificate('fbAdminConfig.json')
-firebase = firebase_admin.initialize_app(cred)
-pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+cred = credentials.Certificate('fbconfig.json')
+firebase = pyrebase.initialize_app(cred)
+auth = firebase.auth()
 
-@app.route('/auth') #endpoint
-def handle_request():
+@app.route('/', methods=['GET','POST']) #endpoint
+def auth():
+    unsuccessful = 'Please check your credentials'
+    successful = 'Login successful'
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            return render_template('new.html', s=successful)
+        except:
+            return render_template('new.html', us=unsuccessful)
 
-    auth = firebase.auth()
-
-    email = request.form['email']
-    password = request.form['password']
-
-    user = firebase.auth().signInWithEmailAndPassword(email, password)
-
-    return "success"
+    return render_template('new.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
