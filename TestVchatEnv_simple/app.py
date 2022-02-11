325e9a036573ec3f26831d43840baf4b08ebddab
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2022-02-10 19:22:05 -0800

    beep

--- TestVchatEnv_simple/app.py
+++ TestVchatEnv_simple/app.py
@@ -3,11 +3,15 @@ from flask import *
 import firebase_admin
 import pyrebase
 from firebase_admin import credentials, auth
+from settings.local_settings import AWS_REGION, ENVIRONMENT
+import json
 app = Flask(__name__)
 
 #connect to firebase
-cred = credentials.Certificate('fbconfig.json')
-firebase = pyrebase.initialize_app(cred)
+###cred = credentials.Certificate('fbconfig.json')
+firebase_config_file='fbconfig.json'
+firebase_admin_creds_file='fbAdminConfig.json'
+firebase = pyrebase.initialize_app(firebase_admin_creds_file)
 auth = firebase.auth()
 
 @app.route('/', methods=['GET','POST']) #endpoint
@@ -19,11 +23,11 @@ def auth():
         password = request.form['password']
         try:
             auth.sign_in_with_email_and_password(email, password)
-            return render_template('new.html', s=successful)
+            return render_template('login.html', s=successful)
         except:
-            return render_template('new.html', us=unsuccessful)
+            return render_template('login.html', us=unsuccessful)
 
-    return render_template('new.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
