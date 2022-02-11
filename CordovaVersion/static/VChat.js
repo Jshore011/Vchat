6b0b5d6aa1b53b66296e3a169bc5001072528fd8
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2022-02-11 14:06:39 -0800

    adding secure logic

--- CordovaVersion/static/VChat.js
+++ CordovaVersion/static/VChat.js
@@ -1,3 +1,44 @@
+var jwt = null
+function secure_get_with_token(endpoint, on_success_callback, on_fail_callback){
+    xhr = new XMLHttpRequest();
+    function setHeader(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer:'+jwt);
+    }
+    function get_and_set_new_jwt(data){
+        console.log(data);
+        jwt  = data.token
+        on_success_callback(data)
+    }
+    $.ajax({
+        url: endpoint,
+        type: 'GET',
+        datatype: 'json',
+        success: on_success_callback,
+        error: on_fail_callback,
+        beforeSend: setHeader
+    });
+}
+
+function secure_get_with_data(endpoint, data_to_send, on_success_callback, on_fail_callback){
+    xhr = new XMLHttpRequest();
+    function setHeader(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer:'+jwt);
+    }
+    function get_and_set_new_jwt(data){
+        console.log(data);
+        jwt  = data.token
+        on_success_callback(data)
+    }
+    $.ajax({
+        url: endpoint,
+        data: data_to_send,
+        type: 'GET',
+        datatype: 'json',
+        success: on_success_callback,
+        error: on_fail_callback,
+        beforeSend: setHeader
+    });
+}
 function pageSwitch({string}){
         switch(loc) {
         case "login_page":
@@ -14,7 +55,7 @@ function pageSwitch({string}){
 
     //$('#dashboard').hide();
    //???? function login() {
-    page_switcher(4);
+    //?????page_switcher(4);
     function login(){
         console.log("in login");
         $.post("/login_auth", {"email":$('#email').val(), "password":$('#password').val()}),
