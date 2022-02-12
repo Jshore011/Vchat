9cffe78f544960ef0e32f115cc16d6ae79b612d7
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2022-02-11 16:01:10 -0800

    Revert "nvm about secure call"
    This reverts commit 883e6737e112ccd838255a91b87d4b4a2e3e4203.

--- CordovaVersion/open_calls/signup_firebase.py
+++ CordovaVersion/open_calls/signup_firebase.py
@@ -1,39 +1,49 @@
 import pyrebase
+
+from getpass import getpass
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+from tools.token_tools import create_token
 import bcrypt
 import psycopg2
 from psycopg2 import sql
+
 from tools.logging import logger
 
 def handle_request():
 
     logging.debug("firebase")
-    config = {
-        "apiKey": "AIzaSyCjMU8ankorPuxvYnxQpbk5hX1a0ZjvwSk",
-        "authDomain": "vchat-37136.firebaseapp.com",
+firebaseConfg ={
+    “apiKey”: "AIzaSyCjMU8ankorPuxvYnxQpbk5hX1a0ZjvwSk",
+
+    “authDomain”: "vchat-37136.firebaseapp.com",
         "databaseURL": "https://vchat-37136.firebaseapp.com/",
-        "projectId": "vchat-37136",
-        "storageBucket": "vchat-37136.appspot.com",
-        "messagingSenderId": "993893663736"
+
+    “projectId”: "vchat-37136",
+
+    “storageBucket”: "vchat-37136.appspot.com",
+
+    “messagingSenderId”: "993893663736",
+
+    “appId”: "1:993893663736:web:87f489696d2f23656dcc0d",
+
+    “measurementId”: "G-7K5G0D6MMW"
     }
 
-    firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-    auth = firebase.auth()
+auth = firebase.auth()
 
-    if request.method == 'POST':
-    		newemail = request.form['newemail']
-    		print("newEmail:")
-    		print(newemail)
+    #pw = request.form['password']
+    #un = request.form['username']
 
-    		newpassword = request.form['newpassword']
-    		print("newPassword:")
-    		print(newpassword)
-    		try:
-    			user=auth.create_user_with_email_and_password(newemail, newpassword)
-    			return  user.get("localId")
-    		except:
-    			return "unsuccessful signup. Try again"
+email = request.form['username']
+logger.debug(email)
+password = request.form['password']
+logger.debug(password)
 
+
+user = auth.create_user_with_email_and_password(email, password)
+
+return "success"
 
