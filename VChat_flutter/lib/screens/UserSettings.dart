0c5bc54d9160ecf37bc12cbbeb34250ee203b852
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2022-02-17 21:16:53 -0800

    bare bones settings page

--- VChat_flutter/lib/screens/UserSettings.dart
+++ VChat_flutter/lib/screens/UserSettings.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vchat_flutter/screens/Home.dart';
-import 'package:vchat_flutter/screens/Wadapak.dart';
-import 'package:vchat_flutter/screens/Signup.dart';
+
 import 'package:vchat_flutter/util/fire_auth.dart';
+
+import 'Profile.dart';
 
 class UserSettingsPage extends StatefulWidget{
   @override
@@ -11,39 +12,156 @@ class UserSettingsPage extends StatefulWidget{
 }
 
 class _UserSettingsPageState extends State<UserSettingsPage> {
+  final _UserSettingsFormKey = GlobalKey<FormState>();
+
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  final _phoneNumberTextController = TextEditingController();
+  final _usernameTextController = TextEditingController();
+  final _nameTextController = TextEditingController();
+
+  final _focusEmail = FocusNode();
+  final _focusPassword = FocusNode();
+  final _focusPhoneNumber = FocusNode();
+  final _focusUsername = FocusNode();
+  final _focusName = FocusNode();
+
+  bool _isProcessing = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     //throw UnimplementedError();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Settings'),
-      ),
-      body: Center(
-        child: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new ElevatedButton(
-              child: const Text('Username'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  throw UnimplementedError,
-                );
-              },
-            ),
-            new ElevatedButton(
-              child: const Text('Email'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  throw UnimplementedError(),
-                );
-              },
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        _focusEmail.unfocus();
+        _focusPassword.unfocus();
+        _focusUsername.unfocus();
+        _focusPassword.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('User Settings'),
         ),
-
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Form(
+                  key: _UserSettingsFormKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _emailTextController,
+                        focusNode: _focusEmail,
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(3.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _passwordTextController,
+                        focusNode: _focusPassword,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: "Password"
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _nameTextController,
+                        focusNode: _focusName,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(3.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _usernameTextController,
+                        focusNode: _focusUsername,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(3.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _phoneNumberTextController,
+                        focusNode: _focusPhoneNumber,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Phone Number",
+                          errorBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(3.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 32.0),
+                      if (_isProcessing) const CircularProgressIndicator() else Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                setState(() {
+                                  _isProcessing = true;
+                                });
+                                if (_UserSettingsFormKey.currentState!
+                                    .validate()) {
+                                  User? user = await FireAuth
+                                      .signInUsingEmailPassword(
+                                    email: _emailTextController.text,
+                                    password:
+                                    _passwordTextController.text, context: context,
+                                  );
+                                  setState(() {
+                                    _isProcessing = false;
+                                  });
+                                  if (user != null) {
+                                    Navigator.of(context)
+                                        .pushAndRemoveUntil(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            NavBarPage(user: user),
+                                      ),
+                                      ModalRoute.withName('/'),
+                                    );
+                                  }
+                                }
+                              },
+                              child: const Text(
+                                'Save Changes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
