e86bca302cec5467e54eef066e27679bca359068
root <root@virtualserver01.VChat.cloud>
2022-04-25 22:22:01 +0000

    Analytics adjusted. still gotta test

--- FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
+++ FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
@@ -6,7 +6,6 @@ import json
 
 from tools.logging import logger
 
-
 def handle_request():
     logger.debug("Analytics Handle Request")
 
@@ -18,10 +17,7 @@ def handle_request():
     chtRmID = requestDetails['conversationID']
     
     cur = g.db.cursor()
-
-    cur.execute(sql.SQL("SELECT COUNT(*) FROM keywords WHERE chtrmid = %s;"),(chtRmID))
-    keyCount = cur.fetchOne()
-    
+       
     #query for most recent message in chatroom
     cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s AND max(orderID);"),(chtRmID))
     msgEmotions = cur.fetchOne()
@@ -29,13 +25,13 @@ def handle_request():
     #query for all user messages in a specific chatroom
     cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s AND usrid = %s;"),(chtRmID,usrID))
     usrEmotions = cur.fetchAll()
-    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s;"),(chtRmID,usrID))
+    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID,usrID))
     usrKeywords = cur.fetchAll()
 
     #query for all messages in a specific chatroom
     cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s;"),(chtRmID))
     chtRmEmotions = cur.fetchAll()
-    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s;"),(chtRmID))
+    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID))
     chtRmKeywords = cur.fetchAll()
     
     data = '{"data":{"msgAnalytics":"'+str(msgEmotions)+'"},{"usrAnalytics":{"emotions:"'+str(usrEmotions)+'"},{"keywords":"'+str(usrKeywords)+'"}},{"chtRmAnalytics":{"emotions:"'+str(chtRmEmotions)+'"},{"keywords":"'+str(chtRmKeywords)+'"}}}'
