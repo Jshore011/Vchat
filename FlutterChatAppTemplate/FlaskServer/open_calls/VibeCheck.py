804f4a17964a0d7abb28ef164fd0e00694eb794a
root <root@virtualserver01.VChat.cloud>
2022-04-25 22:06:48 +0000

    vibecheck updated untested

--- FlutterChatAppTemplate/FlaskServer/open_calls/VibeCheck.py
+++ FlutterChatAppTemplate/FlaskServer/open_calls/VibeCheck.py
@@ -10,30 +10,33 @@ from tools.logging import logger
 def handle_request():
     logger.debug("Vibe Check Handle Request")
 
+    #userID should be sent from mobile app frontend
     usrID = request.get_json()
     
-    #vibe check = top counted top emotion
-
-    #home all chatrooms displayed
-    #--> req Vibecheck
+    #vibe check queries the database for all chatrooms participated in by user
     cur = g.db.cursor()
-    cur.execute(sql.SQL("select chtrmid from chatroom where usrid = %s;"),(usrID))
+    #query gets all chatrooms user participates in                DELETE this query after testing 
+    #cur.execute(sql.SQL("select chtrmid from chatroom where usrid = %s;"),(usrID))
+    
+    #query gets all chatrooms user participates in ordered by recent activity
+    cur.execute(sql.SQL("select DISTINCT chtrmid from messages where usrid = %s ORDER BY orderid DESCENDING;"),(usrID))
     chatrooms = cur.fetchAll()
 
-    data= '{"chatrooms":"'
+    data= '{"chatrooms":['
+
     for c in chatrooms:
-	data += '{"chatroomid":"'+str(chatrooms[c])+'",emotion":'
+	data += '{"chatroomid":"'+str(c)+'","emotion":'
 	current = 0
 	count = 0
 	top = ''
-	emotions = [joy,sadness,fear, anger, disgust]
+	emotions = ['joy','sadness','fear', 'anger', 'disgust']
 	for e in emotions:
-            cur.execute(sql.SQL("select COUNT(*) from messages where chtrmid = %s and topemotion = %s;", (chtRmID[],emotions[e])
+            cur.execute(sql.SQL("select COUNT(*) from messages where chtrmid = %s and topemotion = %s;", (c, e)
 	    current = fetchOne()
 	    if current > count:
                 count = current
-                top = emotions[e]
+                top = e
 	data += '"'+str(top)+'"},'
-    data += '{"None":""}'
+    data += '{"None":""}]}'
 	
     return jsonify(data)
