9e9586585bb0b767104811687d9dace65f87cdab
root <root@virtualserver01.VChat.cloud>
2022-04-25 23:14:10 +0000

    emotion statements all should work, still need to figure out the keyword queries

--- FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
+++ FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
@@ -19,17 +19,17 @@ def handle_request():
     cur = g.db.cursor()
        
     #query for most recent message in chatroom
-    cur.execute(sql.SQL("select emotions from emotions where chtrmid = %s orderid = (select max(orderid) from emotions);"),(chtRmID))
+    cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s orderid = (SELECT MAX(orderid) FROM emotions);"),(chtRmID))
     msgEmotions = cur.fetchOne()
     
     #query for all user messages in a specific chatroom
-    cur.execute(sql.SQL("select topemotion from messages where usrid = %s and chtrmid = %s;"),(chtRmID,usrID))
+    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE usrid = %s AND chtrmid = %s;"),(chtRmID,usrID))
     usrEmotions = cur.fetchAll()
     cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID,usrID))
     usrKeywords = cur.fetchAll()
 
     #query for all messages in a specific chatroom
-    cur.execute(sql.SQL("select topemotion from messages where chtrmid = %s;"),(chtRmID))
+    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE chtrmid = %s;"),(chtRmID))
     chtRmEmotions = cur.fetchAll()
     cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID))
     chtRmKeywords = cur.fetchAll()
