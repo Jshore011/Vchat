cf280999aa942f70e2d1af71f563ae0a9f94135a
root <root@virtualserver01.VChat.cloud>
2022-04-26 20:01:01 +0000

    keyword queries seem to be working, I need more data to be sure though

--- FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
+++ FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
@@ -25,13 +25,15 @@ def handle_request():
     #query for all user messages in a specific chatroom
     cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE usrid = %s AND chtrmid = %s;"),(chtRmID,usrID))
     usrEmotions = cur.fetchAll()
-    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID,usrID))
+    #query for ordered list of most used keywords used by user in chatroom
+    cur.execute(sql.SQL("SELECT DISTINCT keyword, COUNT(keyword) FROM keywords WHERE chtrmid = %s AND usrid = %s GROUP BY keyword ORDER BY COUNT(keyword);"),(chtRmID,usrID))
     usrKeywords = cur.fetchAll()
 
     #query for all messages in a specific chatroom
     cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE chtrmid = %s;"),(chtRmID))
     chtRmEmotions = cur.fetchAll()
-    cur.execute(sql.SQL("SELECT keyword FROM keywords WHERE chtrmid = %s AND usrid = %s ORDER BY COUNT DESCENDING;"),(chtRmID))
+    #query for ordered list of most used keywords in chatroom
+    cur.execute(sql.SQL("SELECT DISTINCT keyword, COUNT(keyword) FROM keywords WHERE chtrmid = %s GROUP BY keyword ORDER BY COUNT(keyword);"),(chtRmID))
     chtRmKeywords = cur.fetchAll()
     
     data = '{"data":{"msgAnalytics":"'+str(msgEmotions)+'"},{"usrAnalytics":{"emotions:"'+str(usrEmotions)+'"},{"keywords":"'+str(usrKeywords)+'"}},{"chtRmAnalytics":{"emotions:"'+str(chtRmEmotions)+'"},{"keywords":"'+str(chtRmKeywords)+'"}}}'
