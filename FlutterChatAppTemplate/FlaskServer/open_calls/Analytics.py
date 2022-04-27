5b3d4477f3d3212159653350ea79ab67f89ca95e
root <root@virtualserver01.VChat.cloud>
2022-04-27 02:33:25 +0000

    sql conversion issue resolved

--- FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
+++ FlutterChatAppTemplate/FlaskServer/open_calls/Analytics.py
@@ -1,4 +1,4 @@
-from flask import request, g
+from flask import request, g, jsonify
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
 from psycopg2 import sql
@@ -9,32 +9,32 @@ from tools.logging import logger
 def handle_request():
     logger.debug("Analytics Handle Request")
 
-    requestDetails = request.get_json()
-    logger.debug(requestDetails)
+    #requestDetails = request.get_json()
+    #logger.debug(requestDetails)
 
-    usrID = requestDetails['usrID']
-    #msgID = requestDetails['messageID']
-    chtRmID = requestDetails['conversationID']
+    usrID = 'ppYgiYl1eKYkm9mX6axlBs0yCqr1'#requestDetails['userID']
+    
+    chtRmID = 'Bmlnlw6bPhsyfSSw9Vta'#requestDetails['conversationID']
     
     cur = g.db.cursor()
        
     #query for most recent message in chatroom
-    cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s orderid = (SELECT MAX(orderid) FROM emotions);"),(chtRmID))
-    msgEmotions = cur.fetchOne()
+    cur.execute(sql.SQL("SELECT emotions FROM emotions WHERE chtrmid = %s AND orderid = (SELECT MAX(orderid) FROM emotions);"),(chtRmID,))
+    msgEmotions = cur.fetchone()
     
     #query for all user messages in a specific chatroom
-    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE usrid = %s AND chtrmid = %s;"),(chtRmID,usrID))
-    usrEmotions = cur.fetchAll()
+    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE usrid = %s AND chtrmid = %s;"),(usrID, chtRmID))
+    usrEmotions = cur.fetchall()
     #query for ordered list of most used keywords used by user in chatroom
     cur.execute(sql.SQL("SELECT DISTINCT keyword, COUNT(keyword) FROM keywords WHERE chtrmid = %s AND usrid = %s GROUP BY keyword ORDER BY COUNT(keyword);"),(chtRmID,usrID))
-    usrKeywords = cur.fetchAll()
+    usrKeywords = cur.fetchall()
 
     #query for all messages in a specific chatroom
-    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE chtrmid = %s;"),(chtRmID))
-    chtRmEmotions = cur.fetchAll()
+    cur.execute(sql.SQL("SELECT topemotion FROM messages WHERE chtrmid = %s;"),(chtRmID,))
+    chtRmEmotions = cur.fetchall()
     #query for ordered list of most used keywords in chatroom
-    cur.execute(sql.SQL("SELECT DISTINCT keyword, COUNT(keyword) FROM keywords WHERE chtrmid = %s GROUP BY keyword ORDER BY COUNT(keyword);"),(chtRmID))
-    chtRmKeywords = cur.fetchAll()
+    cur.execute(sql.SQL("SELECT DISTINCT keyword, COUNT(keyword) FROM keywords WHERE chtrmid = %s GROUP BY keyword ORDER BY COUNT(keyword);"),(chtRmID,))
+    chtRmKeywords = cur.fetchall()
     
     data = '{"data":{"msgAnalytics":"'+str(msgEmotions)+'"},{"usrAnalytics":{"emotions:"'+str(usrEmotions)+'"},{"keywords":"'+str(usrKeywords)+'"}},{"chtRmAnalytics":{"emotions:"'+str(chtRmEmotions)+'"},{"keywords":"'+str(chtRmKeywords)+'"}}}'
     
